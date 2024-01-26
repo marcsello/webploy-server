a6655f0dd659c41a12d80d20563528973884c77f
marcsello <punkosdmarcell@rocketmail.com>
2024-01-26 22:17:43 +0100

    revisited kmutex

--- utils/kmutex.go
+++ utils/kmutex.go
@@ -1,40 +1,58 @@
 package utils
 
-// source: https://medium.com/@petrlozhkin/kmutex-lock-mutex-by-unique-id-408467659c24
-// Sadly, it is not available from the author's GitHub repo, so we have to copy
-
 import (
 	"sync"
+	"time"
 )
 
-type Kmutex struct {
-	locks *sync.Map
+// This is my own lame implementation of KMutex, because I couldn't find any better that
+// - supports arbitrary number of locks (we only store as many as the number of our deployments)
+// - does not have flawed lock logic
+//    - many implementations forget to unlock the lock they use to protect the map, while waiting for a key, essentially creating a global lock
+//    - some other implementations aren't safe from race conditions, because they put unlocked keys in shared maps, which could be stolen by other routines
+// This implementation might not be as efficient, but at least fixes the problems I've found in others...
+
+type KMutex struct {
+	m     *sync.Mutex
+	locks map[string]*sync.Mutex
 }
 
-func NewKmutex() Kmutex {
-	m := sync.Map{}
-	return Kmutex{&m}
-}
-
-func (s *Kmutex) Unlock(key interface{}) {
-	l, exist := s.locks.Load(key)
-	if !exist {
-		panic("kmutex: unlock of unlocked mutex")
+func NewKMutex() *KMutex {
+	return &KMutex{
+		m:     &sync.Mutex{},
+		locks: make(map[string]*sync.Mutex),
 	}
-	l_ := l.(*sync.Mutex)
-	s.locks.Delete(key)
-	l_.Unlock()
 }
 
-func (s *Kmutex) Lock(key interface{}) {
-	m := sync.Mutex{}
-	m_, _ := s.locks.LoadOrStore(key, &m)
-	mm := m_.(*sync.Mutex)
-	mm.Lock()
-	if mm != &m {
-		mm.Unlock()
-		s.Lock(key)
+func (km *KMutex) Lock(key string) {
+	km.m.Lock()
+
+	l, ok := km.locks[key]
+	if !ok {
+		l = &sync.Mutex{}
+		l.Lock()
+		km.locks[key] = l
+		km.m.Unlock()
 		return
 	}
-	return
+	go func() {
+		// we have to make sure we only unlock after the lock is called
+		// So it is not possible to accidentally lock a lock that just been unlocked and lost the reference to it
+		time.Sleep(time.Millisecond * 2)
+		km.m.Unlock()
+	}()
+	l.Lock() // we only store locked keys in the map which are unlocked only when they are removed from the map, so this lock must be locked already
+	l.Unlock()
+
+	// after the lock has been unlocked, retry locking it
+	km.Lock(key)
+}
+
+func (km *KMutex) Unlock(key string) {
+	km.m.Lock()
+	defer km.m.Unlock()
+
+	l := km.locks[key]
+	delete(km.locks, key)
+	l.Unlock()
 }
---
+++ utils/mutex_test.go
@@ -0,0 +1,158 @@
+package utils
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"sync/atomic"
+	"testing"
+	"time"
+)
+
+// Note that exact race conditions are very hard to test, so these tests only test for expected functionality
+
+func TestKMutex_Lock_Simple(t *testing.T) {
+	success := atomic.Bool{}
+	km := NewKMutex()
+
+	km.Lock("test")
+
+	go func() {
+		km.Lock("test")
+		defer km.Unlock("test")
+		success.Store(true)
+	}()
+
+	assert.False(t, success.Load())
+	km.Unlock("test")
+
+	time.Sleep(time.Millisecond * 30)
+	assert.True(t, success.Load())
+
+}
+
+func TestKMutex_Lock_SimpleLock(t *testing.T) {
+	success := atomic.Bool{}
+	km := NewKMutex()
+
+	km.Lock("test")
+
+	go func() {
+		km.Lock("test")
+		defer km.Unlock("test")
+		success.Store(true)
+	}()
+
+	assert.False(t, success.Load())
+	time.Sleep(time.Millisecond * 30)
+	assert.False(t, success.Load())
+
+	km.Unlock("test")
+
+	time.Sleep(time.Millisecond * 30)
+	assert.True(t, success.Load())
+
+}
+
+func TestKMutex_Lock_SimpleOther(t *testing.T) {
+	success := atomic.Bool{}
+	km := NewKMutex()
+
+	km.Lock("test")
+
+	go func() {
+		km.Lock("test2")
+		defer km.Unlock("test2")
+		success.Store(true)
+	}()
+
+	time.Sleep(time.Millisecond * 30)
+	assert.True(t, success.Load())
+
+}
+
+func TestKMutex_Lock_ManySame(t *testing.T) {
+	successCount := atomic.Int32{}
+	km := NewKMutex()
+	step := make(chan byte)
+	proceeded := make(chan byte)
+
+	for i := 0; i < 500; i++ {
+		go func() {
+			km.Lock("test")
+			defer km.Unlock("test")
+			<-step
+			successCount.Add(1)
+			proceeded <- 0
+		}()
+	}
+
+	time.Sleep(time.Millisecond * 30)
+	assert.Equal(t, int32(0), successCount.Load())
+	for i := 0; i < 500; i++ {
+		step <- 0
+		<-proceeded
+		assert.Equal(t, int32(i+1), successCount.Load())
+	}
+
+}
+
+func TestKMutex_Lock_NotBlockingOther(t *testing.T) {
+	oneContinued := atomic.Bool{}
+	otherContinued := atomic.Bool{}
+	km := NewKMutex()
+
+	km.Lock("one")
+
+	go func() {
+		km.Lock("one")
+		defer km.Unlock("one")
+		oneContinued.Store(true)
+	}()
+	go func() {
+		km.Lock("other")
+		defer km.Unlock("other")
+		otherContinued.Store(true)
+	}()
+
+	time.Sleep(time.Millisecond * 10)
+	assert.False(t, oneContinued.Load())
+	assert.True(t, otherContinued.Load())
+
+	km.Unlock("one")
+	time.Sleep(time.Millisecond * 10)
+	assert.True(t, oneContinued.Load())
+	assert.True(t, otherContinued.Load())
+
+}
+
+func TestKMutex_Lock_ManySome(t *testing.T) {
+	successCount := atomic.Int32{}
+	km := NewKMutex()
+	proceeded := make(chan int)
+
+	for i := 0; i < 500; i++ {
+		key := fmt.Sprintf("%d", i)
+		km.Lock(key)
+	}
+
+	for i := 0; i < 500; i++ {
+		go func(id int) {
+			key := fmt.Sprintf("%d", id)
+			km.Lock(key)
+			defer km.Unlock(key)
+			successCount.Add(1)
+			proceeded <- id
+		}(i)
+	}
+
+	time.Sleep(time.Millisecond * 30)
+	assert.Equal(t, int32(0), successCount.Load())
+	for i := 0; i < 500; i++ {
+		key := fmt.Sprintf("%d", i)
+		km.Unlock(key)
+		id := <-proceeded
+		assert.Equal(t, i, id)
+		assert.Equal(t, int32(i+1), successCount.Load())
+	}
+
+}
