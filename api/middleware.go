7eba35518e6b42920528b607f80fa66091d68e94
marcsello <punkosdmarcell@rocketmail.com>
2024-01-26 22:43:02 +0100

    middleware stuffs

--- api/middleware.go
+++ api/middleware.go
@@ -1,32 +1,91 @@
 package api
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 	"net/http"
+	"time"
+	"webploy-server/authentication"
 	"webploy-server/deployment"
 	"webploy-server/site"
 )
 
-const ValidSiteKey = "site"
-const ValidDeploymentKey = "deployment"
+const validSiteKey = "site"
+const validDeploymentKey = "deployment"
+const loggerKey = "lgr"
 
-func ValidSiteMiddleware(siteProvider site.Provider) gin.HandlerFunc {
+func goodLoggerMiddleware(logger *zap.Logger) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		start := time.Now()
+		// some evil middlewares may modify this value, so we store it
+		path := ctx.Request.URL.Path
+
+		subLogger := logger.With(
+			zap.String("method", ctx.Request.Method),
+			zap.String("path", path),
+			zap.String("query", ctx.Request.URL.RawQuery),
+			zap.String("ip", ctx.ClientIP()),
+			zap.String("user-agent", ctx.Request.UserAgent()),
+		)
+
+		ctx.Set(loggerKey, subLogger)
+
+		ctx.Next() // <- execute next thing in the chain
+		end := time.Now()
+
+		latency := end.Sub(start)
+
+		authUser, authOk := authentication.GetAuthenticatedUser(ctx)
+		completedRequestFields := []zapcore.Field{
+			zap.Int("status", ctx.Writer.Status()),
+			zap.Duration("latency", latency),
+			zap.Bool("authOk", authOk),
+		}
+		if authOk {
+			completedRequestFields = append(completedRequestFields, zap.String("authUser", authUser))
+		}
+
+		if len(ctx.Errors) > 0 {
+			// Append error field if this is an erroneous request.
+			for _, e := range ctx.Errors.Errors() {
+				subLogger.Error(e, completedRequestFields...)
+			}
+		}
+
+		subLogger.Info(fmt.Sprintf("%s %s served: %d", ctx.Request.Method, path, ctx.Writer.Status()), completedRequestFields...) // <- always print this
+
+	}
+}
+
+func GetLoggerFromContext(ctx *gin.Context) *zap.Logger { // This one panics
+	var logger *zap.Logger
+	l, ok := ctx.Get(loggerKey)
+	if !ok {
+		panic("could not access logger")
+	}
+	logger = l.(*zap.Logger)
+	return logger
+}
+
+func validSiteMiddleware(siteProvider site.Provider) gin.HandlerFunc {
 
 	return func(ctx *gin.Context) {
 		siteName := ctx.Param("siteName")
 		s, ok := siteProvider.GetSite(siteName)
 		if !ok {
-			// TODO: log
+			GetLoggerFromContext(ctx).Debug("Trying to access a non-existing site")
 			ctx.AbortWithStatus(http.StatusNotFound)
 			return
 		}
 
-		ctx.Set(ValidSiteKey, s)
+		ctx.Set(validSiteKey, s)
 	}
 }
 
 func GetSiteFromContext(ctx *gin.Context) (site.Site, bool) {
-	val, ok := ctx.Get(ValidSiteKey)
+	val, ok := ctx.Get(validSiteKey)
 	if !ok {
 		return nil, false
 	}
@@ -35,12 +94,12 @@ func GetSiteFromContext(ctx *gin.Context) (site.Site, bool) {
 	return s, ok
 }
 
-func ValidDeploymentMiddleware() gin.HandlerFunc {
+func validDeploymentMiddleware() gin.HandlerFunc {
 
 	return func(ctx *gin.Context) {
 		s, ok := GetSiteFromContext(ctx)
 		if !ok {
-			// TODO: log
+			GetLoggerFromContext(ctx).Debug("Trying to access a non-existing site")
 			ctx.AbortWithStatus(http.StatusNotFound)
 			return
 		}
@@ -48,17 +107,17 @@ func ValidDeploymentMiddleware() gin.HandlerFunc {
 		deploymentID := ctx.Param("deploymentID")
 		d, err := s.GetDeployment(deploymentID)
 		if err != nil {
-			// TODO: log
+			GetLoggerFromContext(ctx).Debug("Trying to access a non-existing deployment")
 			ctx.AbortWithStatus(http.StatusNotFound)
 			return
 		}
 
-		ctx.Set(ValidDeploymentKey, d)
+		ctx.Set(validDeploymentKey, d)
 	}
 }
 
 func GetDeploymentFromContext(ctx *gin.Context) (deployment.Deployment, bool) {
-	val, ok := ctx.Get(ValidSiteKey)
+	val, ok := ctx.Get(validSiteKey)
 	if !ok {
 		return nil, false
 	}
